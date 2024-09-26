5580a7077456d4919a7bd0074df4ad0fa594bf29
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-09-26 17:09:28 -0500

    Start work on init subcommand

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-  CLI::App app;
-  CLI11_PARSE(app, argc, argv);
+    CLI::App app{"RAD - Remember Architectural Decisions\nA tool to create and "
+                 "manage your architectural decisions",
+                 "rad"};
 
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+    app.add_subcommand("init", "Initialize RAD in the current directory");
+    CLI11_PARSE(app, argc, argv);
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
