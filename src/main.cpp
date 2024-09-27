71beecd7a513b63e0f5ff24fe80c7c054edce980
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-09-27 10:49:50 -0500

    Add boilerplate for initalizing the app

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include "CLI/CLI.hpp"
+#include <filesystem>
 #include <iostream>
+
+using namespace std;
+using namespace filesystem;
+
+path initializeApp() { return current_path(); }
 
 int main(int argc, char **argv) {
     CLI::App app{"RAD - Remember Architectural Decisions\nA tool to create and "
@@ -7,8 +13,14 @@ int main(int argc, char **argv) {
                  "rad"};
     app.require_subcommand(0, 1);
 
-    app.add_subcommand("init", "Initialize RAD in the current directory");
+    CLI::App *initParser =
+        app.add_subcommand("init", "Initialize RAD in the current directory");
+
     CLI11_PARSE(app, argc, argv);
+
+    if (initParser->parsed()) {
+        initializeApp();
+    }
 
     std::cout << "Hello, World!" << std::endl;
     return 0;
