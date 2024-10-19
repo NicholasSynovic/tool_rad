615281219d2ed0c56380e06ee11331130e17d691
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-10-19 10:52:31 -0500

    Add comments

--- src/config/configFile.h
+++ src/config/configFile.h
@@ -9,22 +9,23 @@ using json = nlohmann::json;
 #ifndef CONFIG_FILE_H
 #define CONFIG_FILE_H
 
-const string CONFIG_FILENAME = ".rad.json";
-const path CONFIG_ADR_DIRECTORY = path("docs/adr");
+const string CONFIG_FILENAME = ".rad.json";         // Config filename
+const path CONFIG_ADR_DIRECTORY = path("docs/adr"); // ADR directory
 
 class ConfigFile {
   private:
-    json DEFAULT_STATE;
+    json DEFAULT_STATE; // The content of the ADR config file
 
   public:
-    path filepath;
+    path filepath; // Path to the ADR config file including the filename
 
-    string adrFormat = "nygard";
+    string adrFormat = "nygard"; // Default format of the ADR file
 
     ConfigFile();
 
     int createConfigFile() const;
     int writeDefaultState() const;
+
     path identifyNearestConfigFilePath() const;
     path findConfigFilePath(path directory) const;
 };
