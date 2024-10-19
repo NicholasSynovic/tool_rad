41f965c90bb8a4466fecc6d0324c3c981ca87eaa
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-10-19 09:10:08 -0500

    Add EdgeX ADR format

--- src/adrs/adrs.h
+++ src/adrs/adrs.h
@@ -20,6 +20,24 @@ class ADR {
     vector<Markdown::section> vs;
 };
 
+class EdgeXADR : public ADR {
+  public:
+    Markdown::section s0 = {"Submitters"};
+    Markdown::section s1 = {"Change Log"};
+    Markdown::section s2 = {"Referenced Use Case(s)"};
+    Markdown::section s3 = {"Context"};
+    Markdown::section s4 = {"Proposed Design"};
+    Markdown::section s5 = {"Considerations"};
+    Markdown::section s6 = {"Decision"};
+    Markdown::section s7 = {"Other Related ADRs"};
+    Markdown::section s8 = {"References"};
+
+    EdgeXADR(int prefix, string title) {
+        t = {prefix, title};
+        vs = {s0, s1, s2, s3, s4, s5, s6, s7, s8};
+    }
+};
+
 class NygardADR : public ADR {
   public:
     Markdown::section s0 = {"Context"};
