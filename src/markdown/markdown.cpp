59780c95b6fe6322c06a7d38cb27bf031c3e2ff3
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-10-19 10:09:54 -0500

    Add comments

--- src/markdown/markdown.cpp
+++ src/markdown/markdown.cpp
@@ -4,19 +4,34 @@
 
 using namespace std;
 
-Markdown::Markdown() {};
+Markdown::Markdown() {}; // TODO: Empty constructor; Do we need this?
 
 string Markdown::createTitle(Markdown::title t) {
+    /**
+     * Convert a `Markdown::title struct` to a string as:
+     * `{PREFIX}. {TITLE}`
+     */
     string title = to_string(t.prefix) + ". " + t.title;
     return title;
 }
 
 string Markdown::createSection(Markdown::section s) {
+    /**
+     * Convert a `Markdown::section struct` to string as:
+     * `{CONTENT}`
+     */
     string section = s.content;
     return section;
 }
 
 string Markdown::createDocument(Markdown::title t, vector<section> vs) {
+    /**
+     * Wrapper around `createTitle` and `createSection` to create a single
+     * string representing the content of a Markdown document
+     * Example:
+     *
+     * `# {TITLE}\n\n## {SECTION}\n\n## {SECTION}`
+     */
     string document = "";
 
     document += "# " + Markdown::createTitle(t) + Markdown::contentSpacer;
--- src/markdown/markdown.h
+++ src/markdown/markdown.h
@@ -9,19 +9,30 @@ using namespace std;
 class Markdown {
   public:
     struct title {
+        /**
+         * Title of the document that will be used both in the:
+         *  - Markdown file name (e.g., `ADR_{PREFIX}_{TITLE}.md`), and
+         *  - Title of the document (e.g., `# {PREFIX}. {TITLE}`).
+         */
         int prefix;
         string title;
     };
 
     struct section {
+        /**
+         * A specific section of the Markdown document.
+         * Example: `## Hello World`
+         */
         string content;
     };
 
-    string contentSpacer = "\n\n";
+    string contentSpacer = "\n\n"; // Content to space out titles and sections
 
-    Markdown();
-    string createTitle(title t);
-    string createSection(section s);
+    Markdown(); // TODO: Empty constructor; figure out if this is necessary
+    string createTitle(title t);     // Convert struct to string
+    string createSection(section s); // Convert struct to string
+
+    // Wrapper around createTitle and createSection calls
     string createDocument(title t, vector<section> vs);
 };
 
